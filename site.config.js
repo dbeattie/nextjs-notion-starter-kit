4a0253c63fd287e07c167b590f5f99304b97d23a
Darren Beattie <darren@fettle.ca>
2021-12-04 10:16:47 -0800

    Update configuration

--- site.config.js
+++ site.config.js
@@ -1,25 +1,26 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '73400e09163d486db7c01fb83f31f2ec',
+                     
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Darren Beattie',
+  domain: 'darrenbeattie.com',
+  author: 'Darren Beattie',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'Personal Blog of Darren Beattie',
+  socialImageTitle: 'Darren Beattie',
+  socialImageSubtitle: '👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'darren_beattie',
+  github: 'dbeattie',
+  linkedin: 'dbeattie',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
